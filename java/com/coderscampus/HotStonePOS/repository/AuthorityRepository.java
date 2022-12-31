bd87eba8a5785c637647cdea24ef8f1980488a99
Mina K. Fahmy <minakaramm1@gmail.com>
2022-12-31 14:38:20 -0500

    DONE

--- java/com/coderscampus/HotStonePOS/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coderscampus.HotStonePOS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coderscampus.HotStonePOS.domain.Authority;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long>{
-
-}
